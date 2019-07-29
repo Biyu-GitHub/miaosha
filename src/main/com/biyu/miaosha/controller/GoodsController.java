53b53a42710af426d9bdd58dd5dd10bc65e0b8ea
Yu Bi <790125037@qq.com>
2019-07-29 10:41:54 +0800

    :globe_with_meridians: 商品列表页
    1. GoodsService
    2. GoodsDao
    3. GoodsVo
    4. GoodsController
    5. goods_list.html

--- src/main/com/biyu/miaosha/controller/GoodsController.java
+++ src/main/com/biyu/miaosha/controller/GoodsController.java
@@ -1,18 +1,16 @@
 package com.biyu.miaosha.controller;
 
-import org.apache.commons.lang3.StringUtils;
+import com.biyu.miaosha.entity.MiaoshaUser;
+import com.biyu.miaosha.redis.RedisService;
+import com.biyu.miaosha.service.GoodsService;
+import com.biyu.miaosha.service.MiaoshaUserService;
+import com.biyu.miaosha.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.biyu.miaosha.entity.MiaoshaUser;
-import com.biyu.miaosha.redis.RedisService;
-import com.biyu.miaosha.service.MiaoshaUserService;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping("/goods")
@@ -24,9 +22,15 @@ public class GoodsController {
     @Autowired
     RedisService redisService;
 
+    @Autowired
+    GoodsService goodsService;
+
     @RequestMapping("/to_list")
     public String list(Model model, MiaoshaUser user) {
         model.addAttribute("user", user);
+        //查询商品列表
+        List<GoodsVo> goodsList = goodsService.listGoodsVo();
+        model.addAttribute("goodsList", goodsList);
         return "goods_list";
     }
 }
---
+++ src/main/com/biyu/miaosha/dao/GoodsDao.java
@@ -0,0 +1,32 @@
+package com.biyu.miaosha.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.biyu.miaosha.entity.MiaoshaGoods;
+import com.biyu.miaosha.vo.GoodsVo;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface GoodsDao {
+    /**
+     * 查询商品列表
+     * miaosha_goods left join goods
+     *
+     * @return
+     */
+    @Select("select g.*,mg.stock_count, mg.start_date, mg.end_date,mg.miaosha_price from miaosha_goods mg left join goods g on mg.goods_id = g.id")
+    public List<GoodsVo> listGoodsVo();
+
+    @Select("select g.*,mg.stock_count, mg.start_date, mg.end_date,mg.miaosha_price from miaosha_goods mg left join goods g on mg.goods_id = g.id where g.id = #{goodsId}")
+    public GoodsVo getGoodsVoByGoodsId(@Param("goodsId") long goodsId);
+
+    @Update("update miaosha_goods set stock_count = stock_count - 1 where goods_id = #{goodsId}")
+    public int reduceStock(MiaoshaGoods g);
+
+}
