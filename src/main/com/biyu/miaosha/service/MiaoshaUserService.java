d54247245611e7998804cd970f270e54eaf568b6
Yu Bi <790125037@qq.com>
2019-08-13 15:41:51 +0800

    :man: 用户对象缓存

--- src/main/com/biyu/miaosha/service/MiaoshaUserService.java
+++ src/main/com/biyu/miaosha/service/MiaoshaUserService.java
@@ -37,17 +37,16 @@ public class MiaoshaUserService {
     public MiaoshaUser getById(long id) {
 
         // 取缓存
-//        MiaoshaUser user = redisService.get(MiaoshaUserKey.getById, "" + id, MiaoshaUser.class);
-//        if (user != null) {
-//            return user;
-//        }
-//        //取数据库
-//        user = miaoshaUserDao.getById(id);
-//        if (user != null) {
-//            redisService.set(MiaoshaUserKey.getById, "" + id, user);
-//        }
-//        return user;
-        return miaoshaUserDao.getById(id);
+        MiaoshaUser user = redisService.get(MiaoshaUserKey.getById, "" + id, MiaoshaUser.class);
+        if (user != null) {
+            return user;
+        }
+        //取数据库
+        user = miaoshaUserDao.getById(id);
+        if (user != null) {
+            redisService.set(MiaoshaUserKey.getById, "" + id, user);
+        }
+        return user;
     }
 
     /**
